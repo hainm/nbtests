4c4b9617794c137cea574b54f67bcdd5ad0a3ee2
Hai Nguyen <hainm.comp@gmail.com>
2016-07-06 21:30:00 -0400

    verify

--- jscommands/cellHasError.js
+++ jscommands/cellHasError.js
@@ -21,7 +21,7 @@ exports.command = function(cellNumber, callback) {
               console.log("something wrong with # " + cellNumber);
               console.log(result);
           }
-        self.assert.ok(result.value.output_type != 'error', "Check that python has no error");
+        self.verify.ok(result.value.output_type != 'error', "Check that python has no error");
       }
       
       
