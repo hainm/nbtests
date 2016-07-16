506f375a1b6c4d0ac601ba2254336d374b226197
Hai Nguyen <hainm.comp@gmail.com>
2016-07-16 14:10:46 -0400

    print code

--- jscommands/cellHasError.js
+++ jscommands/cellHasError.js
@@ -7,9 +7,9 @@ exports.command = function(cellNumber, callback) {
          
          if (cell.output_area.outputs.length > 0) {
              var out = cell.output_area.outputs[0];
-             return {output_type: out.output_type};
+             return {output_type: out.output_type, code: cell.input[0].innerText};
          }else{
-             return {output_type: 'blank'};
+             return {output_type: 'blank', code: cell.input[0].innerText};
          }
     },
 
@@ -18,7 +18,9 @@ exports.command = function(cellNumber, callback) {
     function(result) {
       if (result.value != null) {
         if (result.value.output_type){
-            console.log('output_type', result.value.output_type);
+            if (result.value.output_type == 'error'){
+                console.log('error code', result.value.code);
+            }
             self.verify.ok(result.value.output_type != 'error', "Check that python has no error");
         }
       }
