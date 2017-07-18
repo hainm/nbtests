5542320c3617ecbe4763c0e8f2f3fdf755d269bd
HN <nhai.qn@gmail.com>
2017-07-18 00:09:30 -0400

    print ouput error

--- jscommands/cellHasError.js
+++ jscommands/cellHasError.js
@@ -7,9 +7,11 @@ exports.command = function(cellNumber, callback) {
          
          if (cell.output_area.outputs.length > 0) {
              var out = cell.output_area.outputs[0];
-             return {output_type: out.output_type, code: cell.input[0].innerText};
+             return {output_type: out.output_type, code: cell.input[0].innerText,
+                     out: out};
          }else{
-             return {output_type: 'blank', code: cell.input[0].innerText};
+             return {output_type: 'blank', code: cell.input[0].innerText,
+                     out: out};
          }
     },
 
@@ -20,6 +22,7 @@ exports.command = function(cellNumber, callback) {
         if (result.value.output_type){
             if (result.value.output_type == 'error'){
                 console.log('error code', result.value.code);
+                console.log('error code', result.value.out);
             }
             self.verify.ok(result.value.output_type != 'error', "Check that python has no error");
         }
